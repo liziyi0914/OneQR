b06880089be4e4f4c6f1d914e7f7f12c25123d51
liziyi0914 <liziyi0914@gmail.com>
2019-07-02 11:01:11 +0800

    add more apps

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ export const dva = {
 						name: '付款码',
 						type: 'Pay',
 						color: '#FF9C00',
-						icon: 'pay-circle',
+						icon: 'rmb',
 						action: 'qr',
 						url: window.location.href,
 						text: '打开支付宝、微信支付、QQ钱包扫一扫支付'
@@ -43,6 +43,41 @@ export const dva = {
 						action: 'qr',
 						url: 'https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&f=wallet&a=1&ac=860167D064CA02EB75DAA45709080989BD2828AE0C889B951E6BCC8C78BDD045&u=138029887&n=liziyi0914',
 						text: '打开QQ扫一扫支付'
+					},
+					unionpay: {
+						name: '银联云闪付',
+						type: 'Pay',
+						color: '#DE3232',
+						icon: 'unionpay',
+						action: 'qr',
+						url: 'https://qr.95516.com/00010000/01213270247009520084852952021972',
+						text: '打开银联云闪付扫一扫支付'
+					},
+					cm: {
+						name: '和包支付',
+						type: 'Pay',
+						color: '#D41370',
+						icon: 'cmcc',
+						action: 'qr',
+						url: 'https://p.10086.cn/d?p=0E447593A308C10A9E2C604A52FC5A2E5C5769E7326E37A0D98D2FD07C985D87',
+						text: '打开和包扫一扫支付'
+					},
+					wo: {
+						name: '沃支付',                                                         type: 'Pay',
+						color: '#F29700',
+						icon: 'unicom',
+						action: 'qr',
+						url: 'http://www.unicompayment.com/externalJump/1002/0wTYxvkga7RGXi6Qs6n1xhqpN8D8lQv1YxZMHb8iBD1RKH6ConqRJ0xoqhKfEKKAD6q4b1taPdbk4UtJdElClQ',
+						text: '打开沃支付扫一扫支付'
+					},
+					yi: {
+						name: '翼支付',
+						type: 'Pay',
+						color: '#ED4552',
+						icon: 'telecom',
+						action: 'qr',
+						url: 'http://m.bestpay.com.cn/#/apa/lz4opAWXk6aYD2XCm0face2face',
+						text: '打开翼支付扫一扫支付'
 					}
 				},
 				Friend: {
@@ -86,7 +121,7 @@ export const dva = {
 						name: '网易云音乐',
 						type: 'Friend',
 						color: '#E60026',
-						icon: 'user',
+						icon: 'netease',
 						action: 'jump',
 						url: 'https://m.music.163.com/m/applink/?scheme=orpheus%3A%2F%2Fuser%2F512939924&go=1&res=1',
 						text: '打开网易云音乐扫一扫加好友'
@@ -109,11 +144,29 @@ export const dva = {
 						url: 'http://s.zhihu.com/BM0e1',
 						text: '打开知乎扫一扫加好友'
 					},
+					bilibili: {
+						name: '哔哩哔哩',
+						type: 'Friend',
+						color: '#FD7299',
+						icon: 'bilibili',
+						action: 'qr',
+						url: 'http://space.bilibili.com/8355003',
+						text: '打开哔哩哔哩扫一扫加好友'
+					},
+					coolapk: {
+						name: '酷安',
+						type: 'Friend',
+						color: '#109D58',
+						icon: 'coolapk',
+						action: 'qr',
+						url: 'https://www.coolapk.com/u/678803?from=qr',
+						text: '打开基佬安扫一扫加好友'
+					},
 					yixin: {
 						name: '易信',
 						type: 'Friend',
 						color: '#08D3AB',
-						icon: 'user',
+						icon: 'yixin',
 						action: 'jump',
 						url: 'http://yixin.im/p/NDA0ODEzMS00MDQ4MTMxLTEzNzc1MDExNjQ=',
 						text: '打开易信扫一扫加好友'
--- src/components/AppIcon.js
+++ src/components/AppIcon.js
@@ -5,15 +5,20 @@ import {
 
 class AppIcon extends Component {
 
-	    render() {
-			var { icon, name, color} = this.props;
-			return (
+	IconFont = Icon.createFromIconfontCN({
+		  scriptUrl: '//at.alicdn.com/t/font_1270946_94k2k3e0wun.js',
+	});
+
+	render() {
+		var { icon, name, color} = this.props;
+		var IconF = this.IconFont;
+		return (
 <div style={{textAlign: 'center'}} {...this.props}>
-	<Icon type={icon} style={{fontSize:'1.5em',color:'white',backgroundColor:color,padding:'0.25em',borderRadius:'1.5em'}}/>
+	<IconF type={'icon-'+icon} style={{fontSize:'1.5em',color:'white',backgroundColor:color,padding:'0.25em',borderRadius:'1.5em'}}/>
 	<div style={{fontSize:'1em',fontWeight:'bold'}}>{name}</div>
 </div>
-				);
-		}
+		);
+	}
 }
 
 export default AppIcon;
